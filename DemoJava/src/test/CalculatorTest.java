08f0d7536144b32c80b930743ea4657ef18e0012
Ashwini Aundhe <ashwinikanashetty@gmail.com>
2022-07-05 13:04:56 +0530

    First commit

---
+++ DemoJava/src/test/CalculatorTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class CalculatorTest {
+  @Test
+  public void addTest() {
+	  int a=7;
+	  int b=10;
+	  int expres =17;
+	  int actres=Calculator.add(a,b);
+	  assertEquals(actres,expres);
+ 
+  }
+}
